8a609896bf7d3e90f181857e508d01f44a717bd7
Olivier <info@gaw-r.company>
2021-12-22 18:04:05 +0100

    add comments

--- examples/example01/example01.lua
+++ examples/example01/example01.lua
@@ -1,34 +1,77 @@
+-- Lua configuration file
+-- Characters following "--" are always comment
+-- It's important to respect the punctuation
+-- Notably the {} and ,
+-- There are three kind of fieds:
+
+-- cfg:Default({
+--     ...
+-- })
+
+-- cfg:Variable({
+--     ...
+-- })
+
+-- cfg:Solid({
+--     ...
+-- })
+
+-- cfg:Default contains the initialization
+-- and rendering parameters
+
+-- Each cfg:Variable contains information
+-- About a variable to be reported
+-- The mandatory fields are Name, Index and Type
+-- *The Index is the same as the variable index in
+--              the table variables[] of the skech
+-- *The Type is the variable type to be reported
+-- *The Name is a name to be displayed
+
+
 --TYPES USED BY SERIAL MONITOR
---typecfg enum {
---	eComDataType_Float		= 'f', -- 8 chars
---	eComDataType_U32 		= 'W', -- 8 chars
---	eComDataType_I32 		= 'w', -- 8 chars
---	eComDataType_U16 		= 'I', -- 4 chars
---	eComDataType_I16 		= 'i', -- 4 chars
---	eComDataType_U8 		= 'B', -- 2 chars
---	eComDataType_I8 		= 'b', -- 2 chars
---} eComDataType_t--
+--  typecfg enum {
+--	eComDataType_Float		= 'f',
+--	eComDataType_U32 		= 'W',
+--	eComDataType_I32 		= 'w',
+--	eComDataType_U16 		= 'I',
+--	eComDataType_I16 		= 'i',
+--	eComDataType_U8 		= 'B',
+--	eComDataType_I8 		= 'b',
+--} eComDataType_t
 
 ---------------  CONFIGURATION ---------------------- 
+-- Normally you'll need only to modify the Baudrate
+-- and the Layout on the Default
+----------------------------------------------------- 
 cfg:Default({
     ----------------------
     -- Mandatory fields --
     ----------------------    
-    Baudrate = 230400,
+    Baudrate = 230400, -- Baudrate of the serial port 
+                       -- used by the target
 
-    -- Commands from monitor to target;
+    -- Commands from monitor to target
+    -- Do not modify uykwyd (unless you know what you do)
     SetValue = '#',
     ReportValueOn = '?',
     ReportValueOff = '!',
 
     -- Messages from target to monitor
+    -- Do not modify uykwyd
     IReportValue =        ':',
     IReportTextConsole =  '>',
 
-    --Every sampleTimeHW a variable is sent
+    -- Every sampleTimeHW a variable is sent
+    -- This value is usefull for the monitoring
+    -- application in order to display the right timing
+    -- Do not modify uykwyd
     SampleTimeHW= 0.001,
 
     --- LAYOUT
+    --- On the upper side of the gui are the various
+    --- Grouptabs. Each Grouptabs contains tabs
+    --- that contain the monitored variables arranged
+    --- in rows and colums. One can have several Grouptabs
     GroupTabs= { 
         {-- group0
             Tabs = {
@@ -50,6 +93,7 @@ cfg:Default({
     Vue3D=      false,       -- default = 'false'
     Plot=       true,       -- default = 'false'
 })
+
 
 ---------------
 cfg:Variable({
